cc612c293cf1cff285ce2f723b2d0d8c3969c0a4
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-02-01 05:25:08 +0530

    setup basic project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [tasks, setTasks] = useState([])
+
+  const fetchTasks = async (taskText) => {
+    setIsLoading(true)
+    setError(null)
+
+    try{
+      const response = await fetch('https://custom-hooks-cdbf3-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json')
+
+      if(!response.ok) {
+        throw new Error("Request failed.!")
+      }
+
+      const data = await response.json()
+
+      const loadedTasks = []
+
+      for(const taskKey in data) {
+        loadedTasks.push({id: taskKey, text: data[taskKey].text})
+      }
+
+      setTasks(loadedTasks)
+
+    } catch (err) {
+      setError(err.message || "Something went wrong.!")
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task))
+  }
+
+  return(
+    <Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
