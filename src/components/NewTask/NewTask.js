ac6f9d66f72a9c8e4e4ff660e8461b658657acb7
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-02-01 05:38:44 +0530

    use custom http hook to send the data

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,40 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
+import useHttp from "../../hooks/use-http";
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] =  useState(null)
+    const {isLoading, error, sendRequest: sendTaskRequest} = useHttp()
+
+    const createdTask = (taskText, taskData) => {
+        const generatedId = taskData.name
+        const createdTask = {id: generatedId, text: taskText}
+
+        props.onAddTask(createdTask)
+    }
 
     const enterTaskHandler = async (taskText) => {
-        setIsLoading(true)
-        setError(null)
-
-        try{
-            const response = await fetch('https://custom-hooks-cdbf3-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json', {
-                method: 'POST',
-                body: JSON.stringify({text: taskText}),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            })
-
-            if(!response.ok) {
-                throw new Error("Request failed.!")
-            }
-
-            const data = await response.json()
-
-            const generatedId = data.name
-            const createdTask = {id: generatedId, text: taskText}
-
-            props.onAddTask(createdTask)
-
-        } catch(err) {
-            setError(err.message || "Something went wrong.!")
-        }
-        setIsLoading(false)
+        sendTaskRequest(
+            {
+                url: 'https://custom-hooks-cdbf3-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json', 
+                method: 'POST', 
+                headers: {'Content-Type': 'application/json'}, 
+                body: {text: taskText},
+            },createdTask.bind(null, taskText))
     }
 
     return(
